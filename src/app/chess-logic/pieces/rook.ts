46ad56ea63981ca8a1bff797ca096036046064dc
Bhabuk <132197539+Ftbhabok@users.noreply.github.com>
2024-07-18 06:45:03 +0000

    added moment for rook

---
+++ src/app/chess-logic/pieces/rook.ts
@@ -0,0 +1,17 @@
+import { Color, FENChar, Coords } from '../models';
+import { Piece } from './piece';
+
+export class Rook extends Piece {
+  protected override _FENChar: FENChar;
+  protected override _directions: Coords[] = [
+    { x: 1, y: 0 },
+    { x: -1, y: 0 },
+    { x: 0, y: 1 },
+    { x: 0, y: -1 },
+  ];
+  constructor(private pieceColor: Color) {
+    super(pieceColor);
+    this._FENChar =
+      pieceColor === Color.white ? FENChar.WhiteRook : FENChar.BlackRook;
+  }
+}
