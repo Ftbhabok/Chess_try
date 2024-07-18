b3431178caa532706d63f32ed29862fb377b82ee
Bhabuk <132197539+Ftbhabok@users.noreply.github.com>
2024-07-18 07:32:11 +0000

    working on displaying chess-board

---
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chess-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './chess-board.component.html',
+  styleUrl: './chess-board.component.css'
+})
+export class ChessBoardComponent {
+private chessBoard = new ChessBoard();
+  public chessBoardvie: (FENChar | null)[][] = this.chessBoard.chessBoardView;
+  
+  public get playerColor(): Color {
+    return this.chessBoard.playerColor;
+  }
+}
